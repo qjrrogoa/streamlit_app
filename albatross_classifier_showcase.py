5d1163b734939e89304babcf6b62379c14685db6
Damin Park <qjrrogoa@gmail.com>
2023-08-06 16:07:35 +0900

    plz

--- albatross_classifier_showcase.py
+++ albatross_classifier_showcase.py
@@ -12,15 +12,21 @@ from transformers import BitsAndBytesConfig
 from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel, PeftConfig
 
-
 import streamlit as st
 import requests
 from streamlit_chat import message
+
 from huggingface_hub import login
+
+login(token='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')
+
 
 # In[2]:
 
-login(token='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')
+
+'''
+LoRA parameter를 가져오고 llama2에 연결합니다.
+'''
 peft_model_id = "RAIJAY/albatross_classifier"
 config = PeftConfig.from_pretrained(peft_model_id)
 model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_8bit=True, device_map='auto')
@@ -28,16 +34,6 @@ tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
 # Load the Lora model
 model = PeftModel.from_pretrained(model, peft_model_id)
-
-
-# In[3]:
-
-
-model
-
-
-# In[4]:
-
 
 def evaluate_model(model, tokenizer, device, instruction, input=None, temperature=0.1, top_p=0.75, top_k=40, num_beams=4, max_new_tokens=128):
     prompt_instruction = "### Instruction:\n{}\n" + (f"### Input:\n{input}\n" if input else "") + "### Response:\n"
@@ -56,21 +52,6 @@ def evaluate_model(model, tokenizer, device, instruction, input=None, temperatur
     
     output = tokenizer.decode(generation_output[0])
     return output.split("### Response:")[1].strip()
-
-
-# In[5]:
-
-
-fomc = '''
-Recent indicators suggest that economic activity has been expanding at a moderate pace. Job gains have been robust in recent months, and the unemployment rate has remained low. Inflation remains elevated. The U.S. banking system is sound and resilient. Tighter credit conditions for households and businesses are likely to weigh on economic activity, hiring, and inflation. The extent of these effects remains uncertain. The Committee remains highly attentive to inflation risks. The Committee seeks to achieve maximum employment and inflation at the rate of 2 percent over the longer run. In support of these goals, the Committee decided to raise the target range for the federal funds rate to 5-1/4 to 5-1/2 percent. The Committee will continue to assess additional information and its implications for monetary policy. In determining the extent of additional policy firming that may be appropriate to return inflation to 2 percent over time, the Committee will take into account the cumulative tightening of monetary policy, the lags with which monetary policy affects economic activity and inflation, and economic and financial developments. In addition, the Committee will continue reducing its holdings of Treasury securities and agency debt and agency mortgage-backed securities, as described in its previously announced plans. The Committee is strongly committed to returning inflation to its 2 percent objective.
-In assessing the appropriate stance of monetary policy, the Committee will continue to monitor the implications of incoming information for the economic outlook. The Committee would be prepared to adjust the stance of monetary policy as appropriate if risks emerge that could impede the attainment of the Committee's goals. The Committee's assessments will take into account a wide range of information, including readings on labor market conditions, inflation pressures and inflation expectations, and financial and international developments.
-'''
-
-output = evaluate_model(model, tokenizer, 'cuda', instruction="Predict FOMC' stance toward Interest Rate Dicision (Hawkish/Neutral/Dovish)", input=fomc)
-print(output)
-
-
-# In[ ]:
 
  
 st.header("🦜Albatross")
